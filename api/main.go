b7cc76b91948401105855b246e5e59dadf4222b5
Sam Kreter <samkreter@gmail.com>
2018-03-31 10:56:10 -0700

    removing extra route path

--- api/main.go
+++ api/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 const port = ":8080"
@@ -12,4 +14,24 @@ func main() {
 	router := NewRouter()
 	log.Println("Listing on port ", port)
 	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+
+	}
+
+	return router
 }
