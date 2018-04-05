ed0ec8ea00f48c2a85610698420e42d38c76cfbe
Sam Kreter <samkreter@gmail.com>
2018-04-04 20:23:05 -0700

    Full influxdb dumper

--- ETL-python/run.py
+++ ETL-python/run.py
@@ -10,9 +10,9 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 #Base defaults. No worries on changing these
-POINT_BUFFER = 10
+POINT_BUFFER = 1000 #Number of points to buffer before writtening them to INfluxdb
 INFLUXDB_HOST = "localhost"
-DATABASE_NAME = "mydb"
+DATABASE_NAME = "BitcoinTest"
 API_URL = "http://samkreter.com/api/metrics/{measurement}?start={start}&end={end}"
 
 def GetAPIResponse(url):
@@ -45,6 +45,31 @@ def CreateInfluxdbPoint(measurement, price, timestamp):
         }
 
 
+def AddDataToInfluxdb(client, json_data, measurement):
+    buffered_count = 0
+    point_buffer = []
+
+    logger.info("Processing and adding %d points to the databases.", len(json_data))
+
+    for data_point in json_data:
+        #Tranform the JSON response into the correct format
+        influx_point = CreateInfluxdbPoint(measurement, float(data_point['price']), data_point['timestamp'])
+        
+        point_buffer.append(influx_point)
+
+        #If we have more points than the buffer, 
+        # write all points to the DB and clear the buffer
+        if len(point_buffer) > POINT_BUFFER:
+            print("Adding buffered Points: ", buffered_count)
+            buffered_count += 1
+            client.write_points(point_buffer)
+            point_buffer = []
+    
+    # Make sure to add any points left over that were less than the buffer
+    if  len(point_buffer) > 0:
+        client.write_points(point_buffer)
+
+
 def main():
     #Connect to the Database
     client = InfluxDBClient(INFLUXDB_HOST, 8086, 'root', 'root', DATABASE_NAME)
@@ -53,34 +78,25 @@ def main():
     client.create_database(DATABASE_NAME)
 
     measurement = "bitcoin"
-    start_date = "2013-08-01"
-    end_date = "2014-02-01"
 
-    #Construct the api url
-    url = API_URL.format(measurement = measurement, start = start_date, end = end_date)
-    
-    #Get the Json data from the API
-    json_data = GetAPIResponse(url)
-    if not json_data:
-        sys.exit(1)
+    dates = ["2013-01-01", "2014-01-01", "2015-01-01", "2016-01-01", "2017-01-01", "2018-01-01"]
 
-    logger.info("Processing and adding %d points to the databases.", len(json_data))
+    json_data = []
 
-    buffered_count = 0
-    point_buffer = []
-
-    for data_point in json_data:
-        #Tranform the JSON response into the correct format
-        influx_point = CreateInfluxdbPoint(measurement, data_point['price'], data_point['timestamp'])
+    #Loop through the list of start and end dates
+    for i in range(len(dates) - 1):
         
-        point_buffer.append(influx_point)
+        logger.info("Getting data for start: " + dates[i] + " end: " + dates[i + 1])
+        
+        #Construct the api url
+        url = API_URL.format(measurement = measurement, start = dates[i], end = dates[i + 1])
+        
+        #Get the Json data from the API
+        json_data = GetAPIResponse(url)
+        if not json_data:
+            sys.exit(1)
 
-        #If we have more points than the buffer, 
-        # write all points to the DB and clear the buffer
-        if len(point_buffer) > POINT_BUFFER:
-            buffered_count += 1
-            client.write_points(point_buffer)
-            point_buffer = []
+        AddDataToInfluxdb(client, json_data, measurement)
 
 if __name__ == '__main__':
     main()
